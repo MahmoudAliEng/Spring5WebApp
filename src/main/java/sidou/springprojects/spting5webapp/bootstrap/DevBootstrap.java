06745f209482521c3c6ec8060d0b462148fdbf52
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-01-15 10:57:42 +0100

    Automatically fill h2 db on app startup

--- src/main/java/sidou/springprojects/spting5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/sidou/springprojects/spting5webapp/bootstrap/DevBootstrap.java
@@ -1,4 +1,50 @@
 package sidou.springprojects.spting5webapp.bootstrap;
 
-public class DevBootstrap {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import sidou.springprojects.spting5webapp.model.Author;
+import sidou.springprojects.spting5webapp.model.Book;
+import sidou.springprojects.spting5webapp.repositories.AuthorRepository;
+import sidou.springprojects.spting5webapp.repositories.BookRepository;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    /*public DevBootstrap (AuthorRepository authorRepository, BookRepository bookRepository){
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }*/
+
+    private void initData(){
+        // object 1
+        Author sidaAli = new Author("Sid Ali", "MAHMOUDI");
+        Book java = new Book("Java Design Patterns", "753219", "Oussama");
+        sidaAli.getBooks().add(java);
+        java.getAuthors().add(sidaAli);
+
+        authorRepository.save(sidaAli);
+        bookRepository.save(java);
+
+        // object 2
+        Author oussama = new Author("Oussama", "H.");
+        Book d = new Book("Be on data", "853219", "Oussama");
+        oussama.getBooks().add(d);
+        d.getAuthors().add(oussama);
+
+        authorRepository.save(oussama);
+        bookRepository.save(d);
+    }
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        initData();
+    }
 }
