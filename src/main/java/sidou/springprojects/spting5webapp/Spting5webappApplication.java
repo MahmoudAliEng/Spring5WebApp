6b3171baa8f9d9ad4160af5edba17ec23652e372
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-01-15 09:19:26 +0100

    Build a basic author-book app

---
+++ src/main/java/sidou/springprojects/spting5webapp/Spting5webappApplication.java
@@ -0,0 +1,13 @@
+package sidou.springprojects.spting5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spting5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spting5webappApplication.class, args);
+	}
+
+}
